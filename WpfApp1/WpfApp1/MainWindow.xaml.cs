7e0bab36f46c201d6d1526afe2e68927413a526c
Krystian_Wojakiewicz <Krysh97@gmail.com>
2019-04-16 03:43:47 +0200

    Fetching currency info using CurrencyLayer

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -2,8 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,9 +18,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1 {
+namespace WpfApp1
+{
 
-    public class Person {
+    public class Person
+    {
         public string Name { get; set; }
         public string Age { get; set; }
         public ImageSource Image { get; set; }
@@ -26,10 +31,12 @@ namespace WpfApp1 {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     /// 
-    public partial class MainWindow : Window {
+    public partial class MainWindow : Window
+    {
         public ObservableCollection<Person> Persons { get; set; }
 
-        public MainWindow() {
+        public MainWindow()
+        {
 
             InitializeComponent();
             Persons = new ObservableCollection<Person>(); 
@@ -37,39 +44,79 @@ namespace WpfApp1 {
             listBox.ItemsSource = Persons;
         }
 
-        private void addButton_Click(object sender, RoutedEventArgs e) {
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
 
-            try {
-                if (!String.IsNullOrEmpty(nameText.Text) && !String.IsNullOrEmpty(ageText.Text)) {
+            try
+            {
+                if (!String.IsNullOrEmpty(nameText.Text) && !String.IsNullOrEmpty(ageText.Text))
+                {
                     Int32.Parse(ageText.Text);
                     Persons.Add(new Person { Name = nameText.Text, Age = ageText.Text, Image = image.Source}); 
                     nameText.Clear();
                     ageText.Clear();
                     image.Source = null;
                 } 
-                else {
+                else
+                {
                     MessageBox.Show("Please enter your credentials");
                 }
             } 
-            catch(FormatException) {
+            catch(FormatException)
+            {
                 MessageBox.Show("Age must be a number!");
             }
             
         }
 
-        private void AddImage_Click(object sender, RoutedEventArgs e) {
+        private void AddImage_Click(object sender, RoutedEventArgs e)
+        {
             OpenFileDialog open = new OpenFileDialog();
             open.Title = "Select a picture";
             open.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
               "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
               "Portable Network Graphic (*.png)|*.png";
-            if (open.ShowDialog() == true) {
+            if (open.ShowDialog() == true)
+            {
                 image.Source = new BitmapImage(new Uri(open.FileName)); 
             }
         }
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
             image.Source = Persons[listBox.SelectedIndex].Image;
+        }
+
+        private async void FetchDataBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // fetchTextBox.Text = await getWebPageAsync();
+            await task();
+        }
+
+
+        public Task task()
+        {
+            return Task.Run(() =>
+            {
+               string MY_API_KEY = "94972b2da817e3ee66b676143c07e31a";
+                while (true)
+                {
+                    string responseText;
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://apilayer.net/api/live?format=1&currencies=EUR&access_key=" + MY_API_KEY);
+
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    {
+                        using (StreamReader responseStream = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8")))
+                        {
+                            responseText = responseStream.ReadToEnd();
+                            Dispatcher.Invoke(() => fetchTextBox.Text = responseText);
+                        }
+                    }
+                    Thread.Sleep(3000);
+                    Dispatcher.Invoke(() => fetchTextBox.Clear());
+                }
+                
+            });
         }
     }
 }
